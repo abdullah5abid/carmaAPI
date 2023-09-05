c48502fbbbdd27d030de1e7f767b1e3186cd51a0
abidkhan03 <abidkhan42935@gmail.com>
2023-09-05 15:18:25 +0500

    Email response

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -40,24 +40,22 @@ export class AuthController {
         email: data.email,
       }
     });
-    const lambdaResponse = urlResponse.data
+    const payload = new TextEncoder().encode(JSON.stringify(data));
+    const command = new InvokeCommand({
+      FunctionName: 'UserManagementStack-CreateUserLambda0154A2EB-5ufMqT4E5ntw',
+      Payload: payload,
+    });
+    const response = await this.lambdaClient.send(command);
+    console.log('response', response);
+    this.logger.info(`response for lambda client: ${JSON.stringify(response)}`);
+    this.logger.info(`Raw Lambda response payload: ${response.Payload}`);
+    // Decode the Uint8Array payload response from Lambda back to string
+    const lambdaResponseString = new TextDecoder().decode(response.Payload as Uint8Array);
+    this.logger.info(`Lambda response string: ${lambdaResponseString}`);
+    const lambdaResponse = JSON.parse(lambdaResponseString);
+    lambdaResponse.email = urlResponse.data.email;
     this.logger.info(`Lambda response: ${JSON.stringify(lambdaResponse)}`);
     return lambdaResponse;
-    // const payload = new TextEncoder().encode(JSON.stringify(data));
-    // const command = new InvokeCommand({
-    //   FunctionName: 'UserManagementStack-CreateUserLambda0154A2EB-5ufMqT4E5ntw',
-    //   Payload: payload,
-    // });
-    // const response = await this.lambdaClient.send(command);
-    // console.log('response', response);
-    // this.logger.info(`response for lambda client: ${JSON.stringify(response)}`);
-    // this.logger.info(`Raw Lambda response payload: ${response.Payload}`);
-    // // Decode the Uint8Array payload response from Lambda back to string
-    // const lambdaResponseString = new TextDecoder().decode(response.Payload as Uint8Array);
-    // this.logger.info(`Lambda response string: ${lambdaResponseString}`);
-    // const lambdaResponse = JSON.parse(lambdaResponseString);
-    // this.logger.info(`Lambda response: ${JSON.stringify(lambdaResponse)}`);
-    // return lambdaResponse;
   }
 
   @Post('signin')
@@ -139,57 +137,3 @@ export class AuthController {
     return request.user;
   }
 }
-
-
-
-// import {
-//   Controller,
-//   Body,
-//   Post,
-//   UseGuards,
-//   Get,
-//   Request,
-// } from '@nestjs/common';
-// import { ApiResponse, ApiTags, ApiBearerAuth } from '@nestjs/swagger';
-// import { AuthGuard } from '@nestjs/passport';
-// import { AuthService } from '@modules/auth/auth.service';
-// import { SigninDto } from '@modules/auth/dto/signin.dto';
-// import { SignupDto } from '@modules/auth/dto/signup.dto';
-// import { UsersService } from '@modules/user/user.service';
-// import { IRequest } from '@modules/user/user.interface';
-
-// @Controller('api/auth')
-// @ApiTags('authentication')
-// export class AuthController {
-//   constructor(
-//     private readonly authService: AuthService,
-//     private readonly userService: UsersService,
-//   ) {}
-
-//   @Post('signin')
-//   @ApiResponse({ status: 201, description: 'Successful Login' })
-//   @ApiResponse({ status: 400, description: 'Bad Request' })
-//   @ApiResponse({ status: 401, description: 'Unauthorized' })
-//   async signin(@Body() signinDto: SigninDto): Promise<any> {
-//     const user = await this.authService.validateUser(signinDto);
-//     return await this.authService.createToken(user);
-//   }
-
-//   @Post('signup')
-//   @ApiResponse({ status: 201, description: 'Successful Registration' })
-//   @ApiResponse({ status: 400, description: 'Bad Request' })
-//   @ApiResponse({ status: 401, description: 'Unauthorized' })
-//   async signup(@Body() signupDto: SignupDto): Promise<any> {
-//     const user = await this.userService.create(signupDto);
-//     return await this.authService.createToken(user);
-//   }
-
-//   @ApiBearerAuth()
-//   @UseGuards(AuthGuard())
-//   @Get('me')
-//   @ApiResponse({ status: 200, description: 'Successful Response' })
-//   @ApiResponse({ status: 401, description: 'Unauthorized' })
-//   async getLoggedInUser(@Request() request: IRequest): Promise<any> {
-//     return request.user;
-//   }
-// }
